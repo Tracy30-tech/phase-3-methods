09633b26cf305065e6415f9f2efadbc8d867afdc
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-02-12 14:59:34 +0300

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+ end 
+ 
+ greet_programmer
+ 
+ def greet (name="Trevor")
+     puts "Hello, #{name}!"
+ end
+
+ greet
+ 
+ def greet_with_default (name="programmer")
+     puts "Hello, #{name}!"
+ end   
+ 
+ greet_with_default "Tracy"
+ 
+ def add (num1, num2)
+    return num1 + num2
+ end
+  puts add(2,3) 
+ 
+ def halve(number)
+     if number.class != Integer
+         return nil        
+ end  
+     number/2
+ end    
+ 
+ puts halve(9)   
